648712cc94d28432bad793e8dca2f10f82bc8fc2
HJQ <>
2020-09-08 15:38:35 +0800

    提交

--- flutter_ctrip/lib/navigator/tab_navigator.dart
+++ flutter_ctrip/lib/navigator/tab_navigator.dart
@@ -31,7 +31,8 @@ class _State extends State<TabNavigator>
 
     Future.delayed(Duration(seconds: 1)).then((_) {
       //showDialog();
-      showBottomSheetDialog();
+      //showBottomSheetDialog();
+      _openModalBottomSheet();
     });
   }
 
@@ -80,6 +81,40 @@ class _State extends State<TabNavigator>
                     cancelCallback: () {},
                   )));
         });
+  }
+
+  Future _openModalBottomSheet() async {
+    final option = await showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return Container(
+            height: 200.0,
+            child: Column(
+              children: <Widget>[
+                ListTile(
+                  title: Text('拍照', textAlign: TextAlign.center),
+                  onTap: () {
+                    Navigator.pop(context, '拍照');
+                  },
+                ),
+                ListTile(
+                  title: Text('从相册选择', textAlign: TextAlign.center),
+                  onTap: () {
+                    Navigator.pop(context, '从相册选择');
+                  },
+                ),
+                ListTile(
+                  title: Text('取消', textAlign: TextAlign.center),
+                  onTap: () {
+                    Navigator.pop(context, '取消');
+                  },
+                ),
+              ],
+            ),
+          );
+        });
+
+    print(option);
   }
 
   @override
