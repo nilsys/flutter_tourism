b4c35c028a06edaad29641aa8101499189c763df
HJQ <>
2020-09-18 15:53:49 +0800

    提交

--- flutter_ctrip/lib/demo/utils/list_view_item_type.dart
+++ flutter_ctrip/lib/demo/utils/list_view_item_type.dart
@@ -1,8 +1,8 @@
-
 class ListViewItemType {
-  static final String listViewHeader = "header";
+  static final String listViewHeader = "Header";
   static final String sectionHeader = "sectionHeader";
   static final String listViewItem = "listViewItem";
   static final String sectionFooter = "sectionFooter";
-  static final String listViewfooter = "footer";
+  static final String listViewFooter = "Footer";
+  static final String divider = "divider";
 }
