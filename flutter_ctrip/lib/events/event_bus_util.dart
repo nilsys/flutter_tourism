381d694a86d95c330d39d9fca65982fd9e19f3df
HJQ <>
2020-09-22 09:37:41 +0800

    提交

--- flutter_ctrip/lib/events/event_bus_util.dart
+++ flutter_ctrip/lib/events/event_bus_util.dart
@@ -17,3 +17,38 @@ class PageEvent {
   String test;
   PageEvent(this.test);
 }
+
+/**
+ 使用：
+ 1、第二个页面发送通知(发送订阅消息传值)：EventBusUtil.getInstance().fire(PageEvent('login'));
+ 2、第一个页面监听和移除
+   @override
+  void initState() {
+    super.initState();
+    //监听登录事件
+    _event = EventBusUtil.getInstance().on<PageEvent>().listen((data) {
+      if (data.test == 'login') {
+        setState(() {
+          _lodinState = '通知我登录成功了';
+        });
+      }else{
+        setState(() {
+          _logoutState = '通知我退出登录成功了';
+        });
+      }
+    });
+  }
+
+  EventBusUtil.getInstance().on<PageEvent>().listen((data) {
+                print('${data.test}');
+                sss.cancel();
+              });
+
+ @override
+  void dispose() {
+    super.dispose();
+    //移除监听机制
+    _event.cancel();
+  }
+
+ */
