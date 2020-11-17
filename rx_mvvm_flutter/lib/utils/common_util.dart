962be0fc8b614cf54f2f78ba21d1453c5f8653c8
HJQ <>
2020-11-17 10:56:39 +0800

    增加数据库

--- rx_mvvm_flutter/lib/utils/common_util.dart
+++ rx_mvvm_flutter/lib/utils/common_util.dart
@@ -1,9 +1,11 @@
 class CommonUtil {
+  // 密码正则匹配
   static isPassword(String password) {
     return new RegExp(r'^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,12}$')
         .hasMatch(password);
   }
 
+  // 邮箱正则匹配
   static isEmail(String email) {
     return new RegExp(r'^(\w-*\.*)+@(\w-?)+(\.\w{2,})+$').hasMatch(email);
   }
--- rx_mvvm_flutter/lib/utils/string_util.dart
+++ rx_mvvm_flutter/lib/utils/string_util.dart
@@ -1,14 +1,20 @@
 import 'dart:convert';
 
 class StringUtils {
+  // 字符串编码
   static String encode(String originalCn) {
     return jsonEncode(Utf8Encoder().convert(originalCn));
   }
 
+  // 字符串解码
   static String decode(String encodeCn) {
     var list = List<int>();
     jsonDecode(encodeCn).forEach(list.add);
     String value = Utf8Decoder().convert(list);
     return value;
+  }
+
+  static isEmpty(String str) {
+    return str == null || str.isEmpty || str.length == 0;
   }
 }
