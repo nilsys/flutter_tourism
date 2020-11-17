dd62378995017b62f7c5296266f65913b5cb7789
HJQ <>
2020-11-17 11:45:24 +0800

    提交

--- rx_mvvm_flutter/lib/provide/currentIndex.dart
+++ rx_mvvm_flutter/lib/provide/currentIndex.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
-class CurrentIndexProvide with ChangeNotifier{
-  int currentIndex=0;
+// 可参考：https://www.cnblogs.com/wangjunwei/p/10797864.html
+class CurrentIndexProvide with ChangeNotifier {
+  int currentIndex = 0;
 
-  changeIndex(int newIndex){
-    currentIndex=newIndex;
-    notifyListeners();//通知
+  changeIndex(int newIndex) {
+    currentIndex = newIndex;
+    notifyListeners(); //通知
   }
 }
